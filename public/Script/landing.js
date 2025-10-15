dd053bced71997674e0d7b12b7deab0e38141134
Luyanda <luyanda.s.zuma@gmail.com>
2025-10-15 18:20:52 +0200

    added js script for landing page

---
+++ public/Script/landing.js
@@ -0,0 +1,8 @@
+let loveLinesCardElement = document.querySelector("#love-lines-card");
+loveLinesCardElement.addEventListener("click", () => {window.location.href = "lovelines.html";});
+
+let moodVerseCardElement = document.querySelector("#mood-verse-card");
+moodVerseCardElement.addEventListener("click", () => {window.location.href = "moodverse.html";});
+
+let soulScriptCardElement = document.querySelector("#soul-script-card");
+soulScriptCardElement.addEventListener("click", () => {window.location.href = "soulscript.html";});
